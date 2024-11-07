73f446bdc30c95c3c9e01e1057bf4c2af8034e1b
Cheesemanjd <cheesemanjd26@vmi.edu>
2024-11-07 11:54:22 -0500

    Update Stored_Data.py

--- src/Stored_Data.py
+++ src/Stored_Data.py
@@ -1,1 +1,9 @@
+import PyPDF2
 
+with open("fire.pdf", "rb") as pdf_file:
+    read_pdf = PyPDF2.PdfReader(pdf_file)
+    number_of_pages = len(read_pdf.pages)
+    page = read_pdf.pages[0]
+
+    page_content = page.extract_text()
+print(number_of_pages)
