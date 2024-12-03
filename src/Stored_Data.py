6cf1faf1e03b2fb630bed0b2379d2b32f61721fb
Cheesemanjd <cheesemanjd26@vmi.edu>
2024-12-02 23:41:13 -0500

    Update Stored_Data.py

--- src/Stored_Data.py
+++ src/Stored_Data.py
@@ -1,4 +1,5 @@
 import PyPDF2
+import openai
 
 with open("fire.pdf", "rb") as pdf_file:
     read_pdf = PyPDF2.PdfReader(pdf_file)
@@ -7,3 +8,6 @@ with open("fire.pdf", "rb") as pdf_file:
 
     page_content = page.extract_text()
 print(number_of_pages)
+
+messsage = send_to_openai("Store the following PDF file in memory to reffrance when needed ")
+ai_response = send_to_openai(read_pdf)
